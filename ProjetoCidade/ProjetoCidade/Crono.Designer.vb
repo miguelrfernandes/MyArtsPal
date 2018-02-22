b0724dcd60252403de0a9dea4e68234fbf9cd15f
Unknown <u3402953@mvrht.net>
2018-02-22 11:47:52 +0000

    Museu

--- ProjetoCidade/ProjetoCidade/Crono.Designer.vb
+++ ProjetoCidade/ProjetoCidade/Crono.Designer.vb
@@ -141,7 +141,7 @@ Partial Class Crono
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(334, 423)
+        Me.Button1.Location = New System.Drawing.Point(342, 423)
         Me.Button1.Name = "Button1"
         Me.Button1.Size = New System.Drawing.Size(343, 193)
         Me.Button1.TabIndex = 4
@@ -234,11 +234,11 @@ Partial Class Crono
         Me.Label2.BackColor = System.Drawing.Color.Transparent
         Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Label2.ForeColor = System.Drawing.SystemColors.Control
-        Me.Label2.Location = New System.Drawing.Point(346, 508)
+        Me.Label2.Location = New System.Drawing.Point(321, 508)
         Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(331, 20)
+        Me.Label2.Size = New System.Drawing.Size(395, 20)
         Me.Label2.TabIndex = 14
-        Me.Label2.Text = "Seleciona a data correspondente a essa obra"
+        Me.Label2.Text = "Seleciona a data correspondente à criação dessa obra"
         '
         'Crono
         '
